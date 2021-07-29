8752c0865f1b2b1762af928f1cc060dfd6a1f2d4
rafael-hsm <rafameireles2011@gmail.com>
2021-07-28 23:17:27 -0300

    att

--- Aula 12/ex011.js
+++ Aula 12/ex011.js
@@ -1,6 +1,8 @@
-var idade = 22
-if (idade <18) {
-    console.log(`Menor de idade.`)
+var idade = 68
+if (idade <16) {
+    console.log(`Não vota.`)
+} else if (idade < 18 || idade > 65) {
+    console.log(`Voto opcional.`)
 } else {
-    console.log(`Maior de idade`)
+    console.log(`Voto obrigatório.`)
 }
