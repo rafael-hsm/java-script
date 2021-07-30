07a746adb401a3b444e9e7e58f2e693fc2082d58
rafael-hsm <rafameireles2011@gmail.com>
2021-07-30 13:30:25 -0300

    att

--- Aula 12ex/ex015/script.js
+++ Aula 12ex/ex015/script.js
@@ -8,6 +8,42 @@ function verificar() {
     } else {
         var fsex = document.getElementsByName('radiosex')
         var idade = ano - fano.value
-        res.innerHTML = `Idade calculada: ${idade} `
+        var genero = ``
+        var img = document.createElement('img')
+        img.setAttribute('id', 'foto')
+        if (fsex[0].checked) {
+            genero = 'Homem'
+            if (idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'imagens/menino-min.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/moco-min.png')
+            } else if (idade <50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/homem-min.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/senhor-min.png')
+            } 
+        } else {
+            genero = 'Mulher'
+            if (idade >= 0 && idade <10) {
+                //Criança
+                img.setAttribute('src', 'imagens/menina-min.png')
+            } else if (idade < 21) {
+                //Jovem
+                img.setAttribute('src', 'imagens/moca-min.png')
+            } else if (idade <50) {
+                //Adulto
+                img.setAttribute('src', 'imagens/mulher-min.png')
+            } else {
+                //idoso
+                img.setAttribute('src', 'imagens/senhora-min.png')
+            } 
+        }
+        res.style.textAlign = 'center'
+        res.innerHTML = `Detectamos ${genero} com ${idade} anos.`
+        res.appendChild(img)
     }
 }
