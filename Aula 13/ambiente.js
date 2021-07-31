20866c0b111ff868b131fd2b7032d8f358373699
rafael-hsm <rafameireles2011@gmail.com>
2021-07-31 08:36:13 -0300

    att

---
+++ Aula 13/ambiente.js
@@ -0,0 +1,21 @@
+/*
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+console.log('Tudo bem!')
+*/
+
+/*
+while (c <= 5) {
+    console.log(`Passo ${c}`)
+    c++ // c = c+1
+}
+*/
+
+var c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+} while (c <= 6)
